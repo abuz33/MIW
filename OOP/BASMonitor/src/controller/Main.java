535923f0d59a3c0eafc8940627108fb7f3b36ba3
abuz33 <aalaca.33@gmail.com>
2021-09-24 16:38:09 +0200

    Added new files

--- OOP/BASMonitor/src/controller/Main.java
+++ OOP/BASMonitor/src/controller/Main.java
@@ -1,7 +1,71 @@
 package controller;
 
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println(digital_root(494949499));
+        System.out.println(findIt(new int[]{20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5}));
+
+        System.out.println(revRot("733049910872815764", 5) == "330479108928157");
+    }
+
+    public static long digital_root(long n) {
+        // ...
+//        while (n >= 10) {
+//            String[] nums = String.valueOf(n).split("");
+//            int res = 0;
+//            for (String num : nums) {
+//                res += Integer.parseInt(num);
+//            }
+//            n = res;
+//        }
+
+        return (n != 0 && n % 9 == 0) ? 9 : n % 9;
+    }
+
+    public static int findIt(int[] a) {
+        int XOR = 0;
+        for (int i : a) {
+            XOR ^= i;
+        }
+        return XOR;
+    }
+
+    public static String revRot(String strng, int sz) {
+        // your code
+        String res = "";
+        if (sz <= 0 || strng.equals("") || sz > strng.length()) return "";
+        for (int i = 0; i < strng.length() / sz + 1; i++) {
+            if ((i + 1) * sz > strng.length()) break;
+            String[] nums = strng.substring(i * sz, (i + 1) * sz).split("");
+            int total = 0;
+            for (String num : nums) {
+                total += Integer.parseInt(num);
+            }
+
+            if (total % 2 == 0) {
+                res += new StringBuilder(String.join("", nums)).reverse().toString();
+            } else {
+                res += String.join("", nums).substring(1) + String.join("", nums).substring(0, 1);
+            }
+
+        }
+        return res;
+
+    }
+
+    public static String revRot2(String str, int sz) {
+        // your code
+        Function<String, String> prd = s -> s.chars().map(c -> c - '0').sum() % 2 == 0 ?
+                new StringBuilder(s).reverse().toString() : s.substring(1) + s.substring(0, 1);
+
+        return Arrays.stream(str.split("(?<=\\G.{" + sz + "})"))
+                .filter(s -> s.length() == sz)
+                .map(s -> prd.apply(s))
+                .collect(Collectors.joining());
+
     }
 }
