1036785af31b2411cdcf6669a8d32a9540f44e12
abuz33 <aalaca.33@gmail.com>
2021-10-04 10:21:06 +0200

    Added new files

--- OOP/NamenSorteren/src/controller/Main.java
+++ OOP/NamenSorteren/src/controller/Main.java
@@ -1,8 +1,27 @@
 package controller;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        int aantalNamen = NamenBestandVerwerker.telNamenBestand("9.2 ExtraNamen.txt");
+        int aantalNamen2 = NamenBestandVerwerker.telNamenBestand("9.2 GesorteerdeNamenOrigineel.txt");
+
+        ArrayList<String> sorteerdeNamen = NamenBestandVerwerker.leesNamenBestand("9.2 GesorteerdeNamenOrigineel.txt");
+        ArrayList<String> extraNamen = NamenBestandVerwerker.leesNamenBestand("9.2 ExtraNamen.txt");
+        System.out.printf("Antal sorteerde Namen: %s \nAantal Extra Namen: %s\n", sorteerdeNamen.size(), extraNamen.size());
+
+        NamenBestandVerwerker.voegNaamToeAanGesorteerdeLijst("Zumra", sorteerdeNamen);
+        NamenBestandVerwerker.voegNaamToeAanGesorteerdeLijst("Abuzer", sorteerdeNamen);
+        NamenBestandVerwerker.voegNaamToeAanGesorteerdeLijst("Ramazan", sorteerdeNamen);
+
+        NamenBestandVerwerker.voegLijstToeAanGesorteerdeLijst(sorteerdeNamen, extraNamen);
+
+        NamenBestandVerwerker.maakBestandVanLijst(sorteerdeNamen, "9.2 GesorteerdeNamenNieuw.txt");
+        for (String n : sorteerdeNamen) {
+            System.out.println(n);
+        }
     }
 }
--- OOP/NamenSorteren/src/controller/NamenBestandVerwerker.java
+++ OOP/NamenSorteren/src/controller/NamenBestandVerwerker.java
@@ -1,4 +1,76 @@
 package controller;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class NamenBestandVerwerker {
+    final static String URI = "/Users/macuser/Desktop/JavaProjects/MIW/OOP/NamenSorteren/src/resources/";
+
+    public static int telNamenBestand(String bestandsnaam) {
+        int aantalNamen = 0;
+
+        try {
+            File file = new File(URI + bestandsnaam);
+            Scanner namen = new Scanner(file);
+            while (namen.hasNext()) {
+                namen.next();
+                aantalNamen += 1;
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("No such file exist in \"resources\" folder. Check again.");
+            return 0;
+        }
+
+        return aantalNamen;
+    }
+
+    public static ArrayList<String> leesNamenBestand(String bestandsnaam) {
+        ArrayList<String> namen = new ArrayList<>();
+
+        int aantalNamen = 0;
+
+        try {
+            File file = new File(URI + bestandsnaam);
+            Scanner namen2 = new Scanner(file);
+            while (namen2.hasNext()) {
+                namen.add(namen2.next());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("No such file exist in \"resources\" folder. Check again.");
+        }
+        return namen;
+    }
+
+    public static void voegNaamToeAanGesorteerdeLijst(String naam, ArrayList<String> lijst) {
+        int idx = 0;
+        while (naam.compareTo(lijst.get(idx)) > 0) {
+            idx++;
+            if (idx >= lijst.size()) {
+                lijst.add(idx, naam);
+                return;
+            }
+        }
+
+        lijst.add(idx, naam);
+    }
+
+    public static void voegLijstToeAanGesorteerdeLijst(ArrayList<String> sorteerdeLijst, ArrayList<String> nieuweLijst) {
+        for (String n : nieuweLijst) {
+            voegNaamToeAanGesorteerdeLijst(n, sorteerdeLijst);
+        }
+    }
+
+    public static void maakBestandVanLijst(ArrayList<String> lijst, String bestandsnaam) {
+        File bestand = new File(URI + bestandsnaam);
+        try {
+            FileWriter fw = new FileWriter(bestand);
+            for (String naam : lijst) {
+                fw.write(naam + "\n");
+            }
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
