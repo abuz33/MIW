67088dd400e706d896b386b6a0745972a8e67c40
abuz33 <aalaca.33@gmail.com>
2021-10-08 13:11:25 +0200

    Added new files

---
+++ OOP/ChessGame/src/com/chess/engine/board/Board.java
@@ -0,0 +1,15 @@
+/**
+ * Created by abuzer.alaca on 08/10/2021
+ **/
+
+
+package com.chess.engine.board;
+
+public class Board {
+    public Board() {
+    }
+
+    public Tile getTile(final int tileCoordinate) {
+        return null;
+    }
+}
